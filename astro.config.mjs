e0efb156cad303e2ff0d6413fddf6c24e7642585
“Giovanny <albarracingiovanny@gmail.com>
2023-07-23 21:41:46 -0500

    Adding Netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import { astroImageTools } from "astro-imagetools";
 import react from '@astrojs/react';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [astroImageTools, react()],
-    experimental: {
-        assets: true
-    }
+  integrations: [astroImageTools, react()],
+  experimental: {
+    assets: true
+  },
+  output: "server",
+  adapter: netlify()
 });
